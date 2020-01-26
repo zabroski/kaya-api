260d63860524b70c1453f44b0dbe059a4a87a4bf
Issouf Zabre <sheridanzabre@gmail.com>
2020-01-25 21:09:07 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/deliverers', (req, res) => {
+    const deliverers = [];
+
+    deliverers.push({
+        id:2,
+        lastName:'Toto',
+        firstName: 'Tata',
+        phonenumber:'333-333-3333'
+    });
+
+    deliverers.push({
+        id:4,
+        lastName:'maman',
+        firstName: 'mom',
+        phonenumber:'333-333-3333',
+    });
+
+    res.send(deliverers);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
