7dfbffd6ae8ed4502dee9e53769fba7e0715e932
Issouf Zabre <sheridanzabre@gmail.com>
2020-02-20 18:26:33 -0500

    acept delivery

--- src/index.ts
+++ src/index.ts
@@ -92,82 +92,82 @@ app.get('/deliveries-history', (req, res) => {
 
 
 
-// app.post('/create-delivery/:delivererId', (req, res) => {
-//     createConnection().then(async (connection) => {
-//         const deliverer = await getRepository(Deliverer).findOne(req.params.delivererId); 
-//         // console.log(req.body)
-//         // console.log(req.body.addresses[0].street);
-//         const merchant = await getRepository(Merchant).findOne(1);
+app.post('/create-delivery/:delivererId', (req, res) => {
+    createConnection().then(async (connection) => {
+        const deliverer = await getRepository(Deliverer).findOne(req.params.delivererId); 
+        // console.log(req.body)
+        // console.log(req.body.addresses[0].street);
+        const merchant = await getRepository(Merchant).findOne(1);
 
-//         if(deliverer === undefined) {
-//             await connection.close();
-//             res.send("deliverer not found", 404);
-//         } else {
-//             const delivery = new Delivery();
-//                 delivery.deliverer = deliverer;
-//                 delivery.merchant = merchant;
-//                 delivery.status = "pending"
+        if(deliverer === undefined) {
+            await connection.close();
+            res.send("deliverer not found", 404);
+        } else {
+            const delivery = new Delivery();
+                delivery.deliverer = deliverer;
+                delivery.merchant = merchant;
+                delivery.status = "pending"
                 
-//             const pickUpAddress = new Address();
-//                 pickUpAddress.type = req.body.addresses[0].type;
-//                 pickUpAddress.street = req.body.addresses[0].street;
-//                 pickUpAddress.zipCode = req.body.addresses[0].zipCode;
-//                 pickUpAddress.country = req.body.addresses[0].country;
-//                 pickUpAddress.city =  req.body.addresses[0].city;
+            const pickUpAddress = new Address();
+                pickUpAddress.type = req.body.addresses[0].type;
+                pickUpAddress.street = req.body.addresses[0].street;
+                pickUpAddress.zipCode = req.body.addresses[0].zipCode;
+                pickUpAddress.country = req.body.addresses[0].country;
+                pickUpAddress.city =  req.body.addresses[0].city;
 
-//             const droppOffAddress = new Address();
-//                 droppOffAddress.type = req.body.addresses[0].type;
-//                 droppOffAddress.street = req.body.addresses[0].street;
-//                 droppOffAddress.zipCode = req.body.addresses[0].zipCode;
-//                 droppOffAddress.country = req.body.addresses[0].country;
-//                 droppOffAddress.city =  req.body.addresses[0].city;
-//                 console.log(req.params)
+            const droppOffAddress = new Address();
+                droppOffAddress.type = req.body.addresses[0].type;
+                droppOffAddress.street = req.body.addresses[0].street;
+                droppOffAddress.zipCode = req.body.addresses[0].zipCode;
+                droppOffAddress.country = req.body.addresses[0].country;
+                droppOffAddress.city =  req.body.addresses[0].city;
+                console.log(req.params)
 
-//                 delivery.addresses = [pickUpAddress, droppOffAddress];
-//             // await connection.manager.save(droppOffAddress);
-//             // await connection.manager.save(pickUpAddress);
-//             await connection.manager.save(delivery);
-//             await connection.close();
+                delivery.addresses = [pickUpAddress, droppOffAddress];
+            // await connection.manager.save(droppOffAddress);
+            // await connection.manager.save(pickUpAddress);
+            await connection.manager.save(delivery);
+            await connection.close();
 
-//             res.send(`New delivery with id ${delivery.id} will be delivered by deliverer ${delivery.deliverer.firstName}`)
-//         }
-//     })
-// })
+            res.send(`New delivery with id ${delivery.id} will be delivered by deliverer ${delivery.deliverer.firstName}`)
+        }
+    })
+})
 
-// app.post('/confirm-pickup/:deliveryId',(req, res) => {
-//     createConnection().then(async (connection) => {
-//         const delivery = await getRepository(Delivery).findOne(req.params.deliveryId);
-//         if (delivery === undefined) {
-//             await connection.close();
-//             res.send("delivery not found",404)
-//         } else {
-//            delivery.status = "in transit";
-//            await connection.manager.update(Delivery, req.params.deliveryId, {status: "in transit"})
-//            await connection.close();
+app.post('/confirm-pickup/:deliveryId',(req, res) => {
+    createConnection().then(async (connection) => {
+        const delivery = await getRepository(Delivery).findOne(req.params.deliveryId);
+        if (delivery === undefined) {
+            await connection.close();
+            res.send("delivery not found",404)
+        } else {
+           delivery.status = "in transit";
+           await connection.manager.update(Delivery, req.params.deliveryId, {status: "in transit"})
+           await connection.close();
            
-//            res.send(delivery);
-//         } 
-//     })
+           res.send(delivery);
+        } 
+    })
 
     
-// })
+})
 
-// app.post('/confirm-dropoff/:deliveryId',(req, res) => {
-//     createConnection().then(async (connection) => {
-//     const delivery = await getRepository(Delivery).findOne(req.params.delivererId);
-//     if (delivery  === undefined) {
-//         await connection.close();
-//         res.send("delivery not found", 404)
-//     } else {
-//         delivery.status = "done";
-//         // res.send(delivery)
-//         await connection.manager.update(Delivery, req.params.deliveryId, {status: "done"})
-//         await connection.close();
-//          res.send(delivery)
+app.post('/confirm-dropoff/:deliveryId',(req, res) => {
+    createConnection().then(async (connection) => {
+    const delivery = await getRepository(Delivery).findOne(req.params.delivererId);
+    if (delivery  === undefined) {
+        await connection.close();
+        res.send("delivery not found", 404)
+    } else {
+        delivery.status = "done";
+        // res.send(delivery)
+        await connection.manager.update(Delivery, req.params.deliveryId, {status: "done"})
+        await connection.close();
+         res.send(delivery)
 
-//     }
-//     })
-// });
+    }
+    })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
